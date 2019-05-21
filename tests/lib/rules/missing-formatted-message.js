cf756978174a9fb2935e87f92106b4f7927bf4b1
Blaine Muri <blainemuri@gmail.com>
2019-05-21 10:39:12 -0700

    Fixes an issue where the parser only checked the first child of an html element (#3)

--- tests/lib/rules/missing-formatted-message.js
+++ tests/lib/rules/missing-formatted-message.js
@@ -41,11 +41,11 @@ ruleTester.run("missing-formatted-message", rule, {
       },
       {
         code: `<p>
-              <FormattedMessage
-                id="profile.manageSubscription.expired.message"
-                defaultMessage="You have a free Calm account. You can purchase a Calm Premium subscription to access our full library of content and features."
-              />
-            </p>`
+<FormattedMessage
+  id="profile.manageSubscription.expired.message"
+  defaultMessage="You have a free Calm account. You can purchase a Calm Premium subscription to access our full library of content and features."
+/>
+</p>`
       },
       {
         code: '<div>1</div>'
@@ -79,6 +79,15 @@ ruleTester.run("missing-formatted-message", rule, {
           errors: [
             {
               message: 'text may need translation: "hello"',
+              type: 'Literal',
+            }
+          ]
+        },
+        {
+          code: '<div><span><FormattedMessage id="test" defaultMessage="hello" /></span>Extra Text</div>',
+          errors: [
+            {
+              message: 'text may need translation: "Extra Text"',
               type: 'Literal',
             }
           ]
